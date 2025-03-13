c43bf1a6bb7a587af40c70fcc2afbb4a80ba32b3
Elizabeth M Wolkovich <lizziemetier@gmail.com>
2025-03-13 10:30:15 -0600

    Code for looking at possible size effects on ring width and updated README

--- analysis/mountrainier/treerings_dbh.R
+++ analysis/mountrainier/treerings_dbh.R
@@ -9,6 +9,7 @@ options(stringsAsFactors = FALSE)
 
 # libraries
 library(ggplot2)
+library(ggsave)
 
 wd <- "~/Documents/git/projects/grephon/climategrowthshifts/analysis/mountrainier"
 
@@ -70,8 +71,90 @@ ggplot(psmeto04, aes(x=year, y=rw, color=as.factor(Core))) +
 	ggtitle("PSME at TO04") +
 	facet_wrap(as.numeric(DBH)~.) 
 
+
 ## Next up, just build a loop for each species (PDF) x stand...
+## 
+spbyplotall <- subset(treerings, select=c("Species", "Stand"))
+spbyplot <- spbyplotall[!duplicated(spbyplotall),]
+spbyplot <- spbyplot[with(spbyplot, order(spbyplot$Species)), ]
+
+pdf(paste(wd, "/figures/treerings_groupbyDBH_rw.pdf", sep=""), onefile = TRUE)
+for (i in c(1:nrow(spbyplot)))
+	{
+	speciesplothere <- treerings[which(treerings$Species==spbyplot$Species[i] & 
+		treerings$Stand==spbyplot$Stand[i]),]
+	plotme <- ggplot(speciesplothere, aes(x=year, y=rw, color=as.factor(Core))) +
+		geom_line() +
+		theme_bw() +
+		xlab("year") +
+		ylab("ring width (mm)") +
+		ggtitle(paste(spbyplot$Species[i], "at", spbyplot$Stand[i])) +
+		facet_wrap(as.numeric(DBH)~.) 
+	print(plotme)	
+}
+dev.off()
+
+# Now do it for basal area (aka area of a circle) and then do it for THE INCREMENT ...
+# So far I just reconstrut the DBH ... need to get the two areas and take the DIFFERENCE
+spbyplotplusall <- subset(treerings, select=c("Species", "Stand", "uniqueID"))
+spbyplotplus <- spbyplotplusall[!duplicated(spbyplotplusall),]
+spbyplotplus <- spbyplotplus[with(spbyplotplus, order(spbyplotplus$Species, spbyplotplus$Species)), ]
+# Watch out! Super cheap and ugly code below!
+treeringsextra <- treerings[1,]
+treeringsextra$radconst <- NA
+treeringsextra$rwarea <- NA
+for (i in c(1:nrow(spbyplotplus)))
+	{
+	speciesplothere <- treerings[which(treerings$Species==spbyplotplus$Species[i] & 
+		treerings$Stand==spbyplotplus$Stand[i] & 
+		treerings$uniqueID==spbyplotplus$uniqueID[i]),]
+	speciesplothere$radconst <- NA
+	speciesplothere <- speciesplothere[with(speciesplothere, order(-speciesplothere$year)), ]
+		for (j in c(1:(nrow(speciesplothere)-1)))
+		{ 
+			# First get the radius through time: DBH (cm to mm) divide by 2 minus increment
+			speciesplothere$radconst[1] <- (as.numeric(speciesplothere$DBH[1])*100)/2
+			speciesplothere$radconst[j+1] <- speciesplothere$radconst[j]-speciesplothere$rw[j]
+			# Now get the area (basal area) and takes the difference over time
+			speciesplothere$rwarea[1] <- NA
+			speciesplothere$rwarea[j+1] <- (pi*speciesplothere$radconst[j]^2)-(pi*speciesplothere$radconst[j+1]^2)
+
+		}	
+	treeringsextra <- rbind(treeringsextra, speciesplothere)	
+}
+treeringsextra <- treeringsextra[-1,]
+treeringsextra$radconst <- treeringsextra$radconst/100
+
+pdf(paste(wd, "/figures/treerings_groupbyDBH_areainc.pdf", sep=""), onefile = TRUE, width=10, height=8)
+for (i in c(1:nrow(spbyplot)))
+	{
+	speciesplothere <- treeringsextra[which(treeringsextra$Species==spbyplot$Species[i] & 
+		treeringsextra$Stand==spbyplot$Stand[i]),]
+	plotme <- ggplot(speciesplothere, aes(x=year, y=radconst, color=as.factor(Core))) +
+		geom_line() +
+		theme_bw() +
+		xlab("year") +
+		ylab("basal area increment (mm)") +
+		ggtitle(paste(spbyplot$Species[i], "at", spbyplot$Stand[i])) + 
+		facet_wrap(as.numeric(DBH)~., scales="free") 
+	print(plotme)	
+}
+dev.off()
 
 
+if(FALSE){
+## Growth goes down with increasing basal area (pi*r^2)
+# assume growth declines for each cm of DBH and we want to look at increment change ...
+# I think this is not a good model, I ignore this problem for now
+xhere <- seq(from=500, to=1500, by=0.1)
+grdecline <- -0.2
+growthere <- grdecline*pi*(xhere/(2*pi))^2
+incrementhere <- rep(NA, length(growthere))
+for(i in c(1:length(xhere)))
+{
+	incrementhere[i] <- (xhere[i+1]/2*pi)-(xhere[i]/2*pi)
+}
+plot(growthere~incrementhere)
+}
 
-	
+
