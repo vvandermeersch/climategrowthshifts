753e2d567cfa2333b5733375c452c80335f4a736
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-03-13 11:23:31 -0700

    downloadinh hourly data could be more efficient, if we avoid server preprocessing? not sure

--- analysis/mountrainier/climate/era5land/download_hourly_era5land.py
+++ analysis/mountrainier/climate/era5land/download_hourly_era5land.py
@@ -1,17 +1,46 @@
-library(reticulate)
+import cdsapi
 
-wd <- "~/projects/climategrowthshifts/analysis/mountrainier"
+dataset = "reanalysis-era5-land"
+client = cdsapi.Client(timeout=3600,quiet=True,debug=False)
 
-datafolder <- file.path(wd, 'data', 'climate', 'era5land')
-setwd(datafolder) # folder where files will be downloaded
 
-# Python config
-use_virtualenv(file.path(wd, 'climate', 'era5land', "bcookenv"), required=TRUE)
-cdsapi <- import("cdsapi")
-source_python(file.path(wd, 'climate', 'era5land', "download_era5.py")) # custom script
-
-#Run script
-for(year in as.character(1950:1980)){
-  download_era5land(year = year)
-}
-
+def download_era5land(year):
+  
+  request = {
+    "variable": ["2m_temperature"],
+    "year": year,
+    "month": [
+      "01", "02", "03",
+      "04", "05", "06",
+      "07", "08", "09",
+      "10", "11", "12"
+    ],
+    "day": [
+      "01", "02", "03",
+      "04", "05", "06",
+      "07", "08", "09",
+      "10", "11", "12",
+      "13", "14", "15",
+      "16", "17", "18",
+      "19", "20", "21",
+      "22", "23", "24",
+      "25", "26", "27",
+      "28", "29", "30",
+      "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "netcdf",
+    "download_format": "unarchived",
+    "area": [47, -122, 46, -121.5]
+  }
+  file_name = "hourly_tmp" + "_" + year + ".nc"
+  client.retrieve(dataset, request).download(file_name)
