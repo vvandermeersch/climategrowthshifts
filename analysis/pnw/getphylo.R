6b97b127fb13b85f0ab048313a4e4222e95cff65
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-06-18 17:34:07 -0700

    add some lines to make the tree ultrametric!

--- analysis/pnw/getphylo.R
+++ analysis/pnw/getphylo.R
@@ -8,8 +8,10 @@ options(stringsAsFactors=FALSE)
 # Setting working directory. Add in your own path in an if statement for your file structure
 if(length(grep("lizzie", getwd())>0)) { 
   setwd("~/Documents/git/projects/grephon/climategrowthshifts/analysis/pnw")
-} else if (length(grep("helloworld", getwd()))>0) 
-{setwd("boomboom")
+} else if(length(grep("victor", getwd())>0)) { 
+  setwd("/home/victor/projects/climategrowthshifts/analysis/pnw")
+}  else if (length(grep("helloworld", getwd()))>0) {
+  setwd("boomboom")
 }
 
 library(stringr)
@@ -68,3 +70,129 @@ phy.plants.here <-  drop.tip(phy.genera.here,
 plot(phy.plants.here,cex=1.25)
 plot(phy.plants.here,cex=1.25, type="f")
 
+# Make the tree ultrametric
+run <- FALSE
+if(run){
+  
+  library(chronos)
+  library(doFuture)
+  
+  .droptip <- function(phy, tip) {
+    s <- which(phy$edge[, 2] == tip)
+    phy$edge.length <- phy$edge.length[-s]
+    phy$edge <- phy$edge[-s, ]
+    k <- which(phy$edge > tip)
+    phy$edge[k] <- phy$edge[k] - 1L
+    phy$tip.label <- phy$tip.label[-tip]
+    phy
+  }
+  
+  CVmod <- function(phy, lambda = 10^(-3:2), model = "correlated",
+                    quiet = FALSE, calibration = makeChronosCalib(phy))
+  {
+    cv <- numeric()
+    n <- Ntip(phy)
+    nb <- n - 1L
+    D <- numeric(n)
+    one2n <- 1:n
+    S <- match(one2n, phy$edge[, 2])
+    cal2 <- calibration
+    cal2$node <- cal2$node - 1L
+    
+    ctrl <- chronos.control(dual.iter.max = 400) # added by VV
+    
+    for (l in lambda) {
+      if (!quiet)
+        cat("Running cross-validation with lambda = ", l, "\n", sep = "")
+      chr <- chronos(phy, l, model, quiet = TRUE, calibration = calibration, control = ctrl)
+      for (i in one2n) {
+        if (!quiet)
+          cat("\r    Dropping terminal branch ", i, "/", n, sep = "")
+        trb <- .droptip(phy, i)
+        chrb <- chronos(trb, l, model, quiet = TRUE, calibration = cal2, control = ctrl)
+        s <- S[i]
+        ancb <- phy$edge[s, 1L] - 1L
+        ## ancb is the node# of the direct ancestor of the dropped tip *in* the modified tree
+        btb <- branching.times(chrb)
+        j <- if (ancb == nb + 1L) 1L else 2L
+        xstar <- btb[ancb - nb] * attr(chrb, "rates")[which(trb$edge[, j] == ancb)]
+        D[i] <- (xstar - phy$edge.length[s])^2 / xstar
+      }
+      sD <- sum(D)
+      if (!quiet) cat("   ==>   CV = ", sD, "\n", sep = "")
+      cv <- c(cv, sD)
+    }
+    cbind(lambda = lambda, CV = cv)
+  }
+  
+  # The idea is to find the lambda values with the lowest CV
+  b <- Sys.time()
+  plan(multisession, workers = 2)
+  lambdas <- 10^seq(-6, 0, 0.5)
+  res <- foreach(lambda = lambdas, .combine=rbind, .options.future = list(seed = TRUE)) %dofuture% {
+    
+    resl <- CVmod(phy.plants.here, lambda)
+    resl
+  }
+  plan(sequential);gc()
+  e <- Sys.time()
+  plot(res, type = "o", log = "xy")
+  
+  # We can narrow the search
+  b <- Sys.time()
+  plan(multisession, workers = 4)
+  lambdas <- 10^seq(-6, -5, 0.1)
+  res <- foreach(lambda = lambdas, .combine=rbind, .options.future = list(seed = TRUE)) %dofuture% {
+    
+    resl <- CVmod(phy.plants.here, lambda)
+    resl
+  }
+  plan(sequential);gc()
+  e <- Sys.time()
+  print(e-b)
+  plot(res, type = "o", log = "xy")
+  
+  # And narrow again?
+  b <- Sys.time()
+  plan(multisession, workers = 4)
+  lambdas <- 10^seq(-5.6, -5.4, 0.02)
+  res <- foreach(lambda = lambdas, .combine=rbind, .options.future = list(seed = TRUE)) %dofuture% {
+    
+    resl <- CVmod(phy.plants.here, lambda)
+    resl
+  }
+  plan(sequential);gc()
+  e <- Sys.time()
+  print(e-b)
+  plot(res, type = "o", log = "xy")
+  
+  # And narrow again?
+  b <- Sys.time()
+  plan(multisession, workers = 3)
+  lambdas <- 10^seq(-5.52, -5.48, 0.005)
+  res <- foreach(lambda = lambdas, .combine=rbind, .options.future = list(seed = TRUE)) %dofuture% {
+    
+    resl <- CVmod(phy.plants.here, lambda)
+    resl
+  }
+  plan(sequential);gc()
+  e <- Sys.time()
+  print(e-b)
+  plot(res, type = "o", log = "xy")
+  
+  
+  #Once you choose lambda, you can make the tree ultrametric with chronos
+  
+  # Make it ultrametric
+  ctrl <- chronos.control(dual.iter.max = 1e6)
+  phy.plants.here.ultra <- chronos(phy.plants.here, lambda =  3.162278e-06, model = "correlated", control = ctrl)
+  #log-Lik = -46.6315
+  #PHIIC = 157.26
+  
+  #Resolve multichotomies
+  phy.plants.here.ultra <- multi2di(phy.plants.here.ultra)
+  
+  # Rescale with the fossil information of how old is the phylogeny (My Botryo family was 69.9 Mya)
+  # treeFam_ultraRes <- rescale(treeFam_ultraR, model = "depth", 69.9)
+  
+}
