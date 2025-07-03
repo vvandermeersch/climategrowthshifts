89027cb55fd54ba5ef4de3562cc5fb1781d9c2d6
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-07-03 11:42:46 -0700

    summary of datasets in ITRDB

--- analysis/pnw/itrdb_lookup.R
+++ analysis/pnw/itrdb_lookup.R
@@ -80,3 +80,109 @@ for(f in filenames[which(datasets %in% datasets_filtered)]){
   data_summary <- rbind(data_summary, d_summ)
 }
 saveRDS(data_summary, file = file.path(wd, 'input', 'itrdb', 'datasets_summary_usonly.rds'))
+
+server <- "https://www.ncei.noaa.gov/pub/data/paleo/treering/measurements/northamerica/canada"
+filenames <- unlist(xml2::as_list(httr::content(httr::GET(server))))
+filenames <- filenames[grepl(filenames, pattern = '*.txt$')]
+datasets <- sub("-.*", "", filenames)
+ringwidth_series <- readRDS(file.path(wd, 'input', 'itrdb', 'ringwidth_series_canadaonly.rds'))
+datasets_filtered <- unique(ringwidth_series$dataset)
+data_summary <- data.frame()
+for(f in filenames[which(datasets %in% datasets_filtered)]){
+  
+  download_until_success(file.path(server, f), file.path(tempdir(), f))
+  text <- read.delim(file.path(tempdir(), f), nrows = 350)
+  file.remove(file.path(tempdir(), f))
+  
+  species_name <- get_val(text,'Species_Name')
+  species_code <- get_val(text,'Tree_Species_Code')
+  
+  north_lat <- get_val(text,'Northernmost_Latitude',numeric=TRUE)
+  south_lat <- get_val(text,'Southernmost_Latitude',numeric=TRUE)
+  east_lon <- get_val(text,'Easternmost_Longitude',numeric=TRUE)
+  west_lon <- get_val(text,'Westernmost_Longitude',numeric=TRUE)
+  altitude <- get_val(text,'Elevation',numeric=TRUE)
+  
+  state <- sub("(\\D*).*", "\\1", f)
+  dataset <- sub("-.*", "", f)
+  
+  first_year <- get_val(text,'First_Year',numeric=TRUE)
+  last_year <- get_val(text,'Last_Year',numeric=TRUE)
+  
+  if(is.na(first_year)){
+    first_year <- get_val(text,'Earliest_Year',numeric=TRUE)
+  }
+  if(is.na(last_year)){
+    last_year <- get_val(text,'Most_Recent_Year',numeric=TRUE)
+  }
+  
+  time_unit <- get_val(text,'Time_Unit',numeric=FALSE)
+  
+  subtext <- text[grep(text[,1],pattern='millimeter'),][1]
+  split_line <- unlist(strsplit(subtext, ','))
+  ringwidth_unit <- split_line[grep(split_line,pattern='millimeter')]
+  ringwidth_unit_notes <- sub(".*are ", "", subtext)
+  if(length(ringwidth_unit)==0){ringwidth_unit <- 'unknown'}
+  if(length(ringwidth_unit_notes)==0){ringwidth_unit_notes <- 'none'}
+  
+  d_summ<- data.frame(state = state, dataset = dataset, species_name, species_code, first_year, last_year, ringwidth_unit, time_unit, 
+                      north_lat, south_lat, east_lon, west_lon, altitude)
+  data_summary <- rbind(data_summary, d_summ)
+}
+saveRDS(data_summary, file = file.path(wd, 'input', 'itrdb', 'datasets_summary_canadaonly.rds'))
+
+server <- "https://www.ncei.noaa.gov/pub/data/paleo/treering/measurements/northamerica/mexico"
+filenames <- unlist(xml2::as_list(httr::content(httr::GET(server))))
+filenames <- filenames[grepl(filenames, pattern = '*.txt$')]
+datasets <- sub("-.*", "", filenames)
+ringwidth_series <- readRDS(file.path(wd, 'input', 'itrdb', 'ringwidth_series_mexicoonly.rds'))
+datasets_filtered <- unique(ringwidth_series$dataset)
+data_summary <- data.frame()
+for(f in filenames[which(datasets %in% datasets_filtered)]){
+  
+  download_until_success(file.path(server, f), file.path(tempdir(), f))
+  text <- read.delim(file.path(tempdir(), f), nrows = 350)
+  file.remove(file.path(tempdir(), f))
+  
+  species_name <- get_val(text,'Species_Name')
+  species_code <- get_val(text,'Tree_Species_Code')
+  
+  north_lat <- get_val(text,'Northernmost_Latitude',numeric=TRUE)
+  south_lat <- get_val(text,'Southernmost_Latitude',numeric=TRUE)
+  east_lon <- get_val(text,'Easternmost_Longitude',numeric=TRUE)
+  west_lon <- get_val(text,'Westernmost_Longitude',numeric=TRUE)
+  altitude <- get_val(text,'Elevation',numeric=TRUE)
+  
+  state <- sub("(\\D*).*", "\\1", f)
+  dataset <- sub("-.*", "", f)
+  
+  first_year <- get_val(text,'First_Year',numeric=TRUE)
+  last_year <- get_val(text,'Last_Year',numeric=TRUE)
+  
+  if(is.na(first_year)){
+    first_year <- get_val(text,'Earliest_Year',numeric=TRUE)
+  }
+  if(is.na(last_year)){
+    last_year <- get_val(text,'Most_Recent_Year',numeric=TRUE)
+  }
+  
+  time_unit <- get_val(text,'Time_Unit',numeric=FALSE)
+  
+  subtext <- text[grep(text[,1],pattern='millimeter'),][1]
+  split_line <- unlist(strsplit(subtext, ','))
+  ringwidth_unit <- split_line[grep(split_line,pattern='millimeter')]
+  ringwidth_unit_notes <- sub(".*are ", "", subtext)
+  if(length(ringwidth_unit)==0){ringwidth_unit <- 'unknown'}
+  if(length(ringwidth_unit_notes)==0){ringwidth_unit_notes <- 'none'}
+  
+  d_summ<- data.frame(state = state, dataset = dataset, species_name, species_code, first_year, last_year, ringwidth_unit, time_unit, 
+                      north_lat, south_lat, east_lon, west_lon, altitude)
+  data_summary <- rbind(data_summary, d_summ)
+}
+saveRDS(data_summary, file = file.path(wd, 'input', 'itrdb', 'datasets_summary_mexicoonly.rds'))
+
+
+data_summary_all <- rbind(readRDS(file = file.path(wd, 'input', 'itrdb', 'datasets_summary_usonly.rds')),
+                          readRDS(file = file.path(wd, 'input', 'itrdb', 'datasets_summary_canadaonly.rds')),
+                          readRDS(file = file.path(wd, 'input', 'itrdb', 'datasets_summary_mexicoonly.rds')))
+saveRDS(data_summary_all, file = file.path(wd, 'input', 'itrdb', 'datasets_summary_all.rds'))
