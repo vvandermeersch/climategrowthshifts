d3e2cc5c2a73e7bc0ea2b62d90ba020f3feaa4d7
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-10-10 10:35:19 -0700

    species-specific estimates across climate range

--- analysis/pnwandmore/model/estimates_across_climate.R
+++ analysis/pnwandmore/model/estimates_across_climate.R
@@ -8,10 +8,11 @@ source('functions/custom_functions.R', local=util)
 setwd(wd)
 
 library(ggplot2)
+library(patchwork)
 
-data <- readRDS(file.path(wd, 'output/model', 'data_24sept2025.rds'))
-datasets <- readRDS(file.path(wd, 'output/model', 'datasets_24sept2025.rds'))
-base_samples <- readRDS(file.path(wd, "output/model/samples_24sept2025_partialpooling_2clades_centered_extended.rds"))
+data <- readRDS(file.path(wd, 'output/model', 'data_07oct2025.rds'))
+datasets <- readRDS(file.path(wd, 'output/model', 'datasets_07oct2025.rds'))
+base_samples <- readRDS(file.path(wd, "output/model/samples_07oct2025_partialpooling_2clades_gdd_amjjas.rds"))
 
 # Species
 species <- unique(datasets[,c('species_code', 'species_name')])
@@ -19,6 +20,16 @@ species[species$species_code == 'PISA', 'species_name'] <- "Pinus sabiniana"
 temp <- stringr::str_split_fixed(species$species_name, " ", 3)
 species$species_name_simplified <- paste(temp[,1], temp[,2], sep=" ")
 species <- species[species$species_code %in% data$uniq_species_ids,]
+species$subclade <- NA
+species[grepl('Pinus', species$species_name_simplified), 'subclade'] <- 'Pinus'
+species[grepl('Larix|Pseudotsuga', species$species_name_simplified), 'subclade'] <- 'Laricoideae'
+species[grepl('Picea', species$species_name_simplified), 'subclade'] <- 'Picea'
+species[grepl('Abies|Tsuga', species$species_name_simplified), 'subclade'] <- 'Abietoideae'
+species[grepl('Juniperus|Callitropsis|Calocedrus|Chamaecyparis|Thuja|Taxodium|Sequoiadendron', species$species_name_simplified), 'subclade'] <- 'Cupressaceae'
+species[grepl('Quercus', species$species_name_simplified), 'subclade'] <- 'Quercus'
+species[grepl('Populus', species$species_name_simplified), 'subclade'] <- 'Populus'
+species[grepl('Platanus', species$species_name_simplified), 'subclade'] <- 'Platanus'
+
 
 # Species-level estimate
 betas_climate <- data.frame(
@@ -37,75 +48,163 @@ betas_climate <- data.frame(
 betas_climate <- merge(species, betas_climate)
 
 # Range-wide GDD
-species_gddrange <- readRDS(file.path(wd, 'output', 'climate', 'species_gddrange_experienced.rds'))
+species_gddrange <- readRDS(file.path(wd, 'output', 'climate', 'species_gdd_amjjas_range_experienced.rds'))
 betas_climate$rangegdd_q5 <- betas_climate$rangegdd_q50 <- betas_climate$rangegdd_q95 <- NA
 for(i in 1:nrow(betas_climate)){
   range_gdd <- species_gddrange[[betas_climate[i, 'species_name_simplified']]]
   betas_climate[i, paste0('rangegdd_q', c(5,50,95))] <- as.numeric(quantile(range_gdd, c(0.05,0.5,0.95)))
 }
-ggplot(data = na.omit(betas_climate)) +
-  geom_vline(aes(xintercept = 0), linetype = 'dotted') + 
+gddslopes_gdd <- ggplot(data = na.omit(betas_climate)) +
+  facet_grid(~subclade, scales = 'free_x') +
   geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
   # geom_density_2d(aes(x = rangegdd_q50, y = betagdd_q50, col = as.character(clade), group = clade), alpha = 0.5) + 
   geom_pointrange(aes(x=rangegdd_q50,
                       ymin=betagdd_q5, y=betagdd_q50, ymax=betagdd_q95,
-                      color = as.character(clade)), size = 0.2) +
+                      color = subclade), size = 0.2) +
   geom_pointrange(aes(xmin=rangegdd_q5, x=rangegdd_q50, xmax=rangegdd_q95,
                       y=betagdd_q50,
-                      color = as.character(clade)), size = 0.2) +
-  scale_color_manual(values = c('#27278f', '#278f27')) +
+                      color = subclade), size = 0.2) +
+  scale_color_manual(
+    breaks = c('Pinus', 'Picea', 'Laricoideae', 'Abietoideae', 'Cupressaceae',
+               'Quercus', 'Populus', 'Platanus'), 
+    values = c('#2A4B73', '#6886A6', '#A0C0D9', '#B97AB0', '#8b8b5f',
+               '#2B7A2B', '#66A766', '#A0CFA0')
+  ) +
   # coord_equal(xlim = c(-0.1,0.3), ylim = c(-0.1,0.3)) +
   theme_bw() +
-  theme(legend.position = 'none') +
+  theme(legend.position = 'none',
+        strip.background = element_blank(),
+        panel.grid = element_blank()) +
   labs(x = "Range-wide GDD (Apr.-Sep., °C)", y = "GDD slope")
-ggplot(data = na.omit(betas_climate)) +
-  geom_vline(aes(xintercept = 0), linetype = 'dotted') + 
+
+smslopes_gdd <- ggplot(data = na.omit(betas_climate)) +
+  facet_grid(~subclade, scales = 'free_x') +
   geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
+  # geom_density_2d(aes(x = rangegdd_q50, y = betagdd_q50, col = as.character(clade), group = clade), alpha = 0.5) + 
   geom_pointrange(aes(x=rangegdd_q50,
                       ymin=betasm_q5, y=betasm_q50, ymax=betasm_q95,
-                      color = as.character(clade)), size = 0.2) +
+                      color = subclade), size = 0.2) +
   geom_pointrange(aes(xmin=rangegdd_q5, x=rangegdd_q50, xmax=rangegdd_q95,
                       y=betasm_q50,
-                      color = as.character(clade)), size = 0.2) +
-  scale_color_manual(values = c('#27278f', '#278f27')) +
+                      color = subclade), size = 0.2) +
+  scale_color_manual(
+    breaks = c('Pinus', 'Picea', 'Laricoideae', 'Abietoideae', 'Cupressaceae',
+               'Quercus', 'Populus', 'Platanus'), 
+    values = c('#2A4B73', '#6886A6', '#A0C0D9', '#B97AB0', '#8b8b5f',
+               '#2B7A2B', '#66A766', '#A0CFA0')
+  ) +
   # coord_equal(xlim = c(-0.1,0.3), ylim = c(-0.1,0.3)) +
   theme_bw() +
-  theme(legend.position = 'none') +
-  labs(x = "Range-wide GDD (Apr.-Sep., °C)", y = "Soil moisture slope")
-ggplot(data = na.omit(betas_climate)) +
-  geom_vline(aes(xintercept = 0), linetype = 'dotted') + 
+  theme(legend.position = 'none',
+        strip.background = element_blank(),
+        panel.grid = element_blank()) +
+  labs(x = "Range-wide GDD (Apr.-Sep., °C)", y = "SM slope")
+
+
+vpdslopes_gdd <- ggplot(data = na.omit(betas_climate)) +
+  facet_grid(~subclade, scales = 'free_x') +
   geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
+  # geom_density_2d(aes(x = rangegdd_q50, y = betagdd_q50, col = as.character(clade), group = clade), alpha = 0.5) + 
   geom_pointrange(aes(x=rangegdd_q50,
                       ymin=betavpd_q5, y=betavpd_q50, ymax=betavpd_q95,
-                      color = as.character(clade)), size = 0.2) +
+                      color = subclade), size = 0.2) +
   geom_pointrange(aes(xmin=rangegdd_q5, x=rangegdd_q50, xmax=rangegdd_q95,
                       y=betavpd_q50,
-                      color = as.character(clade)), size = 0.2) +
-  scale_color_manual(values = c('#27278f', '#278f27')) +
+                      color = subclade), size = 0.2) +
+  scale_color_manual(
+    breaks = c('Pinus', 'Picea', 'Laricoideae', 'Abietoideae', 'Cupressaceae',
+               'Quercus', 'Populus', 'Platanus'), 
+    values = c('#2A4B73', '#6886A6', '#A0C0D9', '#B97AB0', '#8b8b5f',
+               '#2B7A2B', '#66A766', '#A0CFA0')
+  ) +
   # coord_equal(xlim = c(-0.1,0.3), ylim = c(-0.1,0.3)) +
   theme_bw() +
-  theme(legend.position = 'none') +
+  theme(legend.position = 'none',
+        strip.background = element_blank(),
+        panel.grid = element_blank()) +
   labs(x = "Range-wide GDD (Apr.-Sep., °C)", y = "VPD slope")
 
-# Density plots
-# betas_samples <- data.frame()
-# for(i in 1:nrow(betas_climate)){
-#   range_gdd <- species_gddrange[[betas_climate[i, 'species_name_simplified']]]
-#   betas_samples_i <- 
-#     data.frame(species_code = betas_climate[i, 'species_code'], clade = betas_climate[i, 'clade'],
-#                median_gdd = as.numeric(quantile(range_gdd, 0.5)),
-#                betagdd = as.numeric(base_samples[[paste0('beta_gdd[', which(data$uniq_species_ids == betas_climate[i, 'species_code']), ']')]]),
-#                betasm = as.numeric(base_samples[[paste0('beta_sm[', which(data$uniq_species_ids == betas_climate[i, 'species_code']), ']')]]),
-#                betavpd = as.numeric(base_samples[[paste0('beta_vpd[', which(data$uniq_species_ids == betas_climate[i, 'species_code']), ']')]]))
-#   betas_samples <- rbind(betas_samples, betas_samples_i)
-# }
-# 
-# ggplot(data = na.omit(betas_samples)) +
-#   geom_vline(aes(xintercept = 0), linetype = 'dotted') + 
-#   geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
-#   geom_point(aes(x = betasm, y = betavpd, col = as.character(clade)), alpha = 0.1, size = 0.3) +
-#   geom_density_2d(aes(x = betasm, y = betavpd, group = species_code), col = 'white', alpha = 1, linewidth = 2) +
-#   geom_density_2d(aes(x = betasm, y = betavpd, col = as.character(clade), group = species_code), alpha = 1, linewidth = 0.5) +
-#   scale_color_manual(values = c('#27278f', '#278f27'))
+comb <- gddslopes_gdd + smslopes_gdd + vpdslopes_gdd + plot_layout(ncol = 1)
+ggsave(comb, file = file.path(wd, 'figures', 'newyork2025', 'model_estimates', 'estimates_againtGDD_subclades.pdf'),
+       height = 200, width = 400, units = 'mm')
+
+# Range-wide SM
+species_smrange <- readRDS(file.path(wd, 'output', 'climate', 'species_sm_mjj_range_experienced.rds'))
+betas_climate$rangesm_q5 <- betas_climate$rangesm_q50 <- betas_climate$rangesm_q95 <- NA
+for(i in 1:nrow(betas_climate)){
+  range_sm <- species_smrange[[betas_climate[i, 'species_name_simplified']]]*100
+  betas_climate[i, paste0('rangesm_q', c(5,50,95))] <- as.numeric(quantile(range_sm, c(0.05,0.5,0.95)))
+}
+gddslopes_sm <- ggplot(data = na.omit(betas_climate)) +
+  facet_grid(~subclade, scales = 'free_x') +
+  geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
+  # geom_density_2d(aes(x = rangegdd_q50, y = betagdd_q50, col = as.character(clade), group = clade), alpha = 0.5) + 
+  geom_pointrange(aes(x=rangesm_q50,
+                      ymin=betagdd_q5, y=betagdd_q50, ymax=betagdd_q95,
+                      color = subclade), size = 0.2) +
+  geom_pointrange(aes(xmin=rangesm_q5, x=rangesm_q50, xmax=rangesm_q95,
+                      y=betagdd_q50,
+                      color = subclade), size = 0.2) +
+  scale_color_manual(
+    breaks = c('Pinus', 'Picea', 'Laricoideae', 'Abietoideae', 'Cupressaceae',
+               'Quercus', 'Populus', 'Platanus'), 
+    values = c('#2A4B73', '#6886A6', '#A0C0D9', '#B97AB0', '#8b8b5f',
+               '#2B7A2B', '#66A766', '#A0CFA0')
+  ) +
+  # coord_equal(xlim = c(-0.1,0.3), ylim = c(-0.1,0.3)) +
+  theme_bw() +
+  theme(legend.position = 'none',
+        strip.background = element_blank(),
+        panel.grid = element_blank()) +
+  labs(x = "Range-wide SM (May-June, %)", y = "GDD slope")
+
+smslopes_sm <- ggplot(data = na.omit(betas_climate)) +
+  facet_grid(~subclade, scales = 'free_x') +
+  geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
+  # geom_density_2d(aes(x = rangegdd_q50, y = betagdd_q50, col = as.character(clade), group = clade), alpha = 0.5) + 
+  geom_pointrange(aes(x=rangesm_q50,
+                      ymin=betasm_q5, y=betasm_q50, ymax=betasm_q95,
+                      color = subclade), size = 0.2) +
+  geom_pointrange(aes(xmin=rangesm_q5, x=rangesm_q50, xmax=rangesm_q95,
+                      y=betasm_q50,
+                      color = subclade), size = 0.2) +
+  scale_color_manual(
+    breaks = c('Pinus', 'Picea', 'Laricoideae', 'Abietoideae', 'Cupressaceae',
+               'Quercus', 'Populus', 'Platanus'), 
+    values = c('#2A4B73', '#6886A6', '#A0C0D9', '#B97AB0', '#8b8b5f',
+               '#2B7A2B', '#66A766', '#A0CFA0')
+  ) +
+  # coord_equal(xlim = c(-0.1,0.3), ylim = c(-0.1,0.3)) +
+  theme_bw() +
+  theme(legend.position = 'none',
+        strip.background = element_blank(),
+        panel.grid = element_blank()) +
+  labs(x = "Range-wide SM (May-June, %)", y = "SM slope")
 
 
+vpdslopes_sm <- ggplot(data = na.omit(betas_climate)) +
+  facet_grid(~subclade, scales = 'free_x') +
+  geom_hline(aes(yintercept = 0), linetype = 'dotted') + 
+  # geom_density_2d(aes(x = rangegdd_q50, y = betagdd_q50, col = as.character(clade), group = clade), alpha = 0.5) + 
+  geom_pointrange(aes(x=rangesm_q50,
+                      ymin=betavpd_q5, y=betavpd_q50, ymax=betavpd_q95,
+                      color = subclade), size = 0.2) +
+  geom_pointrange(aes(xmin=rangesm_q5, x=rangesm_q50, xmax=rangesm_q95,
+                      y=betavpd_q50,
+                      color = subclade), size = 0.2) +
+  scale_color_manual(
+    breaks = c('Pinus', 'Picea', 'Laricoideae', 'Abietoideae', 'Cupressaceae',
+               'Quercus', 'Populus', 'Platanus'), 
+    values = c('#2A4B73', '#6886A6', '#A0C0D9', '#B97AB0', '#8b8b5f',
+               '#2B7A2B', '#66A766', '#A0CFA0')
+  ) +
+  # coord_equal(xlim = c(-0.1,0.3), ylim = c(-0.1,0.3)) +
+  theme_bw() +
+  theme(legend.position = 'none',
+        strip.background = element_blank(),
+        panel.grid = element_blank()) +
+  labs(x = "Range-wide SM (May-June, %)", y = "VPD slope")
+
+comb <- gddslopes_sm + smslopes_sm + vpdslopes_sm + plot_layout(ncol = 1)
+ggsave(comb, file = file.path(wd, 'figures', 'newyork2025', 'model_estimates', 'estimates_againtSM_subclades.pdf'),
+       height = 200, width = 400, units = 'mm')
