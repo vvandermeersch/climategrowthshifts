7df6841c32ea54f6f011d0612e69edfb546c06cf
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2026-04-17 09:51:48 -0700

    Doug fir in Colorada - diagnostics & figures

--- analysis/pnwandmore/model/shocks/model17/diagnostic_1species.R
+++ analysis/pnwandmore/model/shocks/model17/diagnostic_1species.R
@@ -10,32 +10,32 @@ setwd(wd)
 
 folder <- '/home/victor/projects/climategrowthshifts/analysis/pnwandmore/output/model/'
 
-data <- readRDS(file.path(folder,'data_26mar2026_PIPOsub_19202024.rds'))
-fit_prev <- readRDS(file.path(folder, 'model17/fit_model17_HSGP_PIPOsub_pi0.rds'))
-fit <- readRDS(file.path(folder, 'model17/model17_HSGP_PIPOsub.rds'))
+data <- readRDS(file.path(folder,'data_06apr2026_PSME_colorado_19202024.rds'))
+fit <- readRDS(file.path(folder, 'model17/fit_model17_HSGP_PSME_colorado_pi0.rds'))
 gc()
 fit$diagnostic_summary()
 
 # Load samples
 samples <- fit$draws(c('alpha', 'beta_gdd', 'beta_vpd', 'beta_pre',
-                       
+
                        'rho_sp', 'gamma_sp',
                        'rho_sh', 'gamma_sh',
                        'rho_ind', 'gamma_ind',
-                       
+
                        'tau_sck',
                        'phi_sck0', 'tau_phi_sck', 'alpha_phi_sck',
                        'omega_conc_sck0', 'tau_omega_conc_sck', 'alpha_omega_conc_sck',
-                       
-                       'pi_idsc_sck',
-                       
+
+                       # 'pi_idsc_sck',
+
                        'sigma'
 ))
 names <- dimnames(samples)$variable
-samples <- lapply(1:dim(samples)[3], 
-                  function(k){t(matrix(samples[1:dim(samples)[1],1:dim(samples)[2],k], 
+samples <- lapply(1:dim(samples)[3],
+                  function(k){t(matrix(samples[1:dim(samples)[1],1:dim(samples)[2],k],
                                        nrow = dim(samples)[1], ncol = dim(samples)[2]))})
 names(samples) <- names
+# samples <- readRDS(file.path(folder, 'model17/base_samples_model17_HSGP_PSMEexcept2_pi0.rds'))
 util$check_all_expectand_diagnostics(samples)
 
 # Intercept and slopes
@@ -56,22 +56,18 @@ lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 # Gaussian processes
 par(mfrow = c(3,2))
 util$plot_expectand_pushforward(samples[['rho_sp']], 50, flim= c(10,80), 'rho_sp')
-prior <- rlnorm(1e6, 3.55, 0.24)
-lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 prior <- rlnorm(1e6, 3.7, 0.35)
-lines(density(prior), col = util$c_mid_teal, lwd = 2, lty = 1)
-util$plot_expectand_pushforward(samples[['gamma_sp']], 50, flim= c(0,3), 'gamma_sp')
+lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
+util$plot_expectand_pushforward(samples[['gamma_sp']], 50, flim= c(0,1), 'gamma_sp')
 prior <- rnorm(1e6, 0, log(10) / 2.57)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 util$plot_expectand_pushforward(samples[['rho_sh']], 50, flim= c(1,10), 'rho_sh')
-prior <- rlnorm(1e6, 1.7, 0.26)
+prior <- rlnorm(1e6, 0.4, 0.3)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
-util$plot_expectand_pushforward(samples[['gamma_sh']], 50, flim= c(0,2), 'gamma_sh')
+util$plot_expectand_pushforward(samples[['gamma_sh']], 50, flim= c(0,1), 'gamma_sh')
 prior <- rnorm(1e6, 0, log(3) / 2.57)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 util$plot_expectand_pushforward(samples[['rho_ind']], 50, flim= c(1,8), 'rho_ind')
-prior <- rlnorm(1e6, 0.80, 0.40)
-lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 prior <- rlnorm(1e6, 1.4, 0.35)
 lines(density(prior), col = util$c_mid_teal, lwd = 2, lty = 1)
 util$plot_expectand_pushforward(samples[['gamma_ind']], 50, flim= c(0,1), 'gamma_ind')
@@ -82,30 +78,28 @@ util$plot_pairs_by_chain(samples[['gamma_ind']], 'gamma_ind',
                          samples[['rho_ind']], 'rho_ind')
 
 # Shocks
-par(mfrow = c(4,2))
-util$plot_expectand_pushforward(samples[['tau_sck']], 50, flim= c(0,6), 'tau_sck')
-prior <- rnorm(1e6, 0, log(20) / 2.57)
-lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
+par(mfrow = c(3,2))
+util$plot_expectand_pushforward(samples[['tau_sck']], 50, flim= c(0,5), 'tau_sck')
 prior <- rnorm(1e6, 0, 5 / 2.57)
-lines(density(prior), col = util$c_mid_teal, lwd = 2, lty = 1)
+lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 plot.new()
 util$plot_expectand_pushforward(samples[['phi_sck0']], 50, flim= c(0,1), 'phi_sck0')
 prior <- rbeta(1e6, 2, 20)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
-util$plot_expectand_pushforward(samples[['tau_phi_sck']], 50, flim= c(0,5), 'tau_phi_sck')
+util$plot_expectand_pushforward(samples[['tau_phi_sck']], 50, flim= c(0,3), 'tau_phi_sck')
 prior <- rnorm(1e6, 0, 1)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 util$plot_expectand_pushforward(samples[['omega_conc_sck0']], 50, flim= c(0,1), 'omega_conc_sck0')
-prior <- rbeta(1e6, 30, 20)
-lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 prior <- rbeta(1e6, 12.28, 34)
+lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
+prior <- rbeta(1e6, 3, 17)
 lines(density(prior), col = util$c_mid_teal, lwd = 2, lty = 1)
-util$plot_expectand_pushforward(samples[['tau_omega_conc_sck']], 50, flim= c(0,5), 'tau_omega_conc_sck')
+util$plot_expectand_pushforward(samples[['tau_omega_conc_sck']], 50, flim= c(0,3), 'tau_omega_conc_sck')
 prior <- rnorm(1e6, 0, 1)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
-util$plot_expectand_pushforward(samples[['pi_idsc_sck']], 50, flim= c(0,1), 'pi_idsc_sck')
-prior <- rbeta(1e6, 2, 100)
-lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
+# util$plot_expectand_pushforward(samples[['pi_idsc_sck']], 50, flim= c(0,1), 'pi_idsc_sck')
+# prior <- rbeta(1e6, 2, 100)
+# lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 
 
 shocks <- fit$draws(c('phi_sck', 'omega_conc_sck'))
@@ -115,7 +109,7 @@ shocks <- lapply(1:dim(shocks)[3],
                                        nrow = dim(shocks)[1], ncol = dim(shocks)[2]))})
 names(shocks) <- names
 par(mfrow = c(2,1), mar = c(4,4,1,1))
-util$plot_disc_pushforward_quantiles(shocks, paste0('phi_sck[', 1:data$N_stands, ']'), display_ylim = c(0,0.7),
+util$plot_disc_pushforward_quantiles(shocks, paste0('phi_sck[', 1:data$N_stands, ']'), display_ylim = c(0,1),
                                      ylab = 'p(stand conc.)')
 util$plot_disc_pushforward_quantiles(shocks, paste0('omega_conc_sck[', 1:data$N_stands, ']'), display_ylim = c(0,1),
                                      ylab = 'p(tree sck | stand conc.)')
@@ -124,26 +118,27 @@ util$plot_disc_pushforward_quantiles(shocks, paste0('omega_conc_sck[', 1:data$N_
 
 
 # Generate predictions!
-options(cmdstanr_output_dir = file.path(folder, 'model17', 'tmp'))
-mod_gq <- cmdstan_model(file.path(wd, 'model/stan/model17/hsgp/archive', 'model17_HSGP_indGP_only1species_newpriors_pi0_withGQ.stan'))
-data_gq <- data
-data_gq$grainsize <-  ceiling(data_gq$N_stands/4)
-data_gq$uniq_tree_ids <- NULL
-data_gq$uniq_species_ids <- NULL
-data_gq$uniq_stand_ids <- NULL
-data_gq$uniq_stand_lat <- NULL
-data_gq$uniq_stand_lon <- NULL
-fit_gq <- mod_gq$generate_quantities(fit, data = data_gq, seed = 5838293, parallel_chains = 2)
-fit_gq$save_object(file = file.path(folder, 'fit_gq_model17/model17_HSGP_PIPOsub_pi0.rds'))
-gc()
-gq_samples <- fit_gq$draws(c('num_conc_sck_stdlvl', 'avg_exp_m1_delta_sck_stdlvl', 
-                             'avg_exp_m1_clim_stdlvl'))
-gc()
-names <- dimnames(gq_samples)$variable
-gq_samples <- lapply(1:dim(gq_samples)[3], 
-                  function(k){t(matrix(gq_samples[1:dim(gq_samples)[1],1:dim(gq_samples)[2],k], 
-                                       nrow = dim(gq_samples)[1], ncol = dim(gq_samples)[2]))})
-names(gq_samples) <- names
+# options(cmdstanr_output_dir = file.path(folder, 'model17', 'tmp'))
+# mod_gq <- cmdstan_model(file.path(wd, 'model/stan/model17/hsgp/archive', 'model17_HSGP_indGP_only1species_newpriors_pi0_withGQ.stan'))
+# data_gq <- data
+# data_gq$grainsize <-  ceiling(data_gq$N_stands/4)
+# data_gq$uniq_tree_ids <- NULL
+# data_gq$uniq_species_ids <- NULL
+# data_gq$uniq_stand_ids <- NULL
+# data_gq$uniq_stand_lat <- NULL
+# data_gq$uniq_stand_lon <- NULL
+# fit_gq <- mod_gq$generate_quantities(fit, data = data_gq, seed = 5838293, parallel_chains = 2)
+# fit_gq$save_object(file = file.path(folder, 'fit_gq_model17/model17_HSGP_PIPOsub_pi0.rds'))
+# gc()
+# gq_samples <- fit_gq$draws(c('num_conc_sck_stdlvl', 'avg_exp_m1_delta_sck_stdlvl', 
+#                              'avg_exp_m1_clim_stdlvl'))
+# gc()
+# names <- dimnames(gq_samples)$variable
+# gq_samples <- lapply(1:dim(gq_samples)[3], 
+#                   function(k){t(matrix(gq_samples[1:dim(gq_samples)[1],1:dim(gq_samples)[2],k], 
+#                                        nrow = dim(gq_samples)[1], ncol = dim(gq_samples)[2]))})
+# names(gq_samples) <- names
+gq_samples <- readRDS(file.path(folder, 'model17/standpred_samples_model17_HSGP_PSME_colorado_pi0.rds'))
 gc()
 
 stand_samples <- fit$draws(c('f_sh'))
@@ -198,7 +193,7 @@ for(t in 1:data$N_trees){
 }
 dev.off()
 
-pdf(file.path(wd, 'model/shocks/model17/figures', 'subPIPO_without_idiosyncracy_propshocks.pdf'), height = 13, width = 10)
+pdf(file.path(wd, 'model/shocks/model17/figures', 'PSME_coloarado_without_idiosyncracy_propshocks.pdf'), height = 13, width = 10)
 par(mfrow = c(10,2), mar = c(2.5,2.8,1,1), mgp = c(1.5,0.5,0),
     cex.lab = 0.9, cex.axis = 0.75, cex.main = 0.9)
 util$plot_disc_pushforward_quantiles(shocks, paste0('phi_sck[', 1:data$N_stands, ']'), display_ylim = c(0,1),
--- analysis/pnwandmore/model/shocks/model17/diagnostic_multispecies.R
+++ analysis/pnwandmore/model/shocks/model17/diagnostic_multispecies.R
@@ -11,7 +11,7 @@ setwd(wd)
 folder <- '/home/victor/projects/climategrowthshifts/analysis/pnwandmore/output/model/'
 
 data <- readRDS(file.path(folder,'data_26mar2026_WestCoast_19202024.rds'))
-fit <- readRDS(file.path(folder, 'model17/fit_HSGP_26mar2026_WestCoast_19202024.rds'))
+fit <- readRDS(file.path(folder, 'model17/fit_HSGP_26mar2026_WestCoast_19202024_pi0.rds'))
 gc()
 fit$diagnostic_summary()
 
@@ -33,12 +33,12 @@ samples <- fit$draws(c('mu_alpha', 'tau_alpha', 'alpha',
                        'tau_sck', 'mu_tau_sck', 'tau_tau_sck',
                        'phi_sck0', 'mu_phi_sck', 'tau_phi_sck', 'alpha_phi_sck',
                        'omega_conc_sck0', 'tau_omega_conc_sck', 'alpha_omega_conc_sck',
-                       'pi_idsc_sck0', 'tau_pi_idsc_sck', 'alpha_pi_idsc_sck',
+                       # 'pi_idsc_sck0', 'tau_pi_idsc_sck', 'alpha_pi_idsc_sck',
                        
                        'sigma'
 ))
 names <- dimnames(samples)$variable
-chains <- 1:3
+chains <- 1:4
 samples <- lapply(1:dim(samples)[3], 
                   function(k){t(matrix(samples[1:dim(samples)[1],chains,k], 
                                        nrow = dim(samples)[1], ncol =  length(chains)))})
@@ -47,6 +47,8 @@ util$check_all_expectand_diagnostics(samples)
 
 
 # Intercept and slopes
+util$check_all_expectand_diagnostics(util$filter_expectands(samples, c('mu_alpha', 'tau_alpha', 'alpha'),
+                                                            check_arrays = T))
 layout(matrix(c(1,2,3,3), ncol = 2, byrow = T))
 par(mar = c(1,4,4,1), cex.lab = 1)
 util$plot_expectand_pushforward(samples[['mu_alpha']], 50, flim= c(-2,2), main = 'mu_alpha')
@@ -59,6 +61,9 @@ par(mar = c(3,4,2,1))
 util$plot_disc_pushforward_quantiles(samples, paste0('alpha[',1:data$N_species,']'),
                                      ylab = 'alpha')
 
+
+util$check_all_expectand_diagnostics(util$filter_expectands(samples, c('mu_gdd', 'tau_gdd', 'beta_gdd'),
+                                                            check_arrays = T))
 layout(matrix(c(1,2,3,3), ncol = 2, byrow = T))
 par(mar = c(1,4,4,1), cex.lab = 1)
 util$plot_expectand_pushforward(samples[['mu_gdd']], 50, flim= c(-0.3,0.3), main = 'mu_gdd')
@@ -71,6 +76,8 @@ par(mar = c(3,4,2,1))
 util$plot_disc_pushforward_quantiles(samples, paste0('beta_gdd[',1:data$N_species,']'),
                                      ylab = 'beta_gdd', display_ylim = c(-0.1, 0.1))
 
+util$check_all_expectand_diagnostics(util$filter_expectands(samples, c('mu_pre', 'tau_pre', 'beta_pre'),
+                                                            check_arrays = T))
 layout(matrix(c(1,2,3,3), ncol = 2, byrow = T))
 par(mar = c(1,4,4,1), cex.lab = 1)
 util$plot_expectand_pushforward(samples[['mu_pre']], 50, flim= c(-0.3,0.3), main = 'mu_pre')
@@ -83,6 +90,8 @@ par(mar = c(3,4,2,1))
 util$plot_disc_pushforward_quantiles(samples, paste0('beta_pre[',1:data$N_species,']'),
                                      ylab = 'beta_pre', display_ylim = c(-0.1, 0.1))
 
+util$check_all_expectand_diagnostics(util$filter_expectands(samples, c('mu_vpd', 'tau_vpd', 'beta_vpd'),
+                                                            check_arrays = T))
 layout(matrix(c(1,2,3,3), ncol = 2, byrow = T))
 par(mar = c(1,4,4,1), cex.lab = 1)
 util$plot_expectand_pushforward(samples[['mu_vpd']], 50, flim= c(-0.3,0.3), main = 'mu_vpd')
@@ -149,7 +158,7 @@ prior <- rnorm(1e6, 0, 0.2 / 2.57)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 par(mar = c(3,4,2,1))
 util$plot_disc_pushforward_quantiles(samples, paste0('kappa_sh[',1:data$N_species,']'),
-                                     ylab = 'kappa_sh', display_ylim = c(0, 5))
+                                     ylab = 'kappa_sh', display_ylim = c(0, 1))
 
 
 # Shocks
@@ -163,24 +172,34 @@ prior <- rnorm(1e6, 0, 1 / 2.57)
 lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
 par(mar = c(3,4,2,1))
 util$plot_disc_pushforward_quantiles(samples, paste0('tau_sck[',1:data$N_species,']'),
-                                     ylab = 'tau_sck', display_ylim = c(0, 3))
+                                     ylab = 'tau_sck', display_ylim = c(0, 4))
 
 
-shocks <- fit$draws(c('phi_sck', 'omega_conc_sck', 'pi_idsc_sck'))
+shocks <- fit$draws(c('phi_sck', 'omega_conc_sck'))
 names <- dimnames(shocks)$variable
 shocks <- lapply(1:dim(shocks)[3], 
                  function(k){t(matrix(shocks[1:dim(shocks)[1],1:dim(shocks)[2],k], 
                                       nrow = dim(shocks)[1], ncol = dim(shocks)[2]))})
 names(shocks) <- names
-par(mfrow = c(3,1), mar = c(4,4,1,1))
-util$plot_disc_pushforward_quantiles(shocks, paste0('phi_sck[', 1:data$N_stands, ']'), display_ylim = c(0,0.7),
+par(mfrow = c(2,1), mar = c(4,4,1,1))
+util$plot_disc_pushforward_quantiles(shocks, paste0('phi_sck[', 1:data$N_stands, ']'), display_ylim = c(0,1),
                                      ylab = 'p(stand conc.)')
 util$plot_disc_pushforward_quantiles(shocks, paste0('omega_conc_sck[', 1:data$N_stand_species, ']'), display_ylim = c(0,1),
                                      ylab = 'p(tree sck | stand conc.)')
 util$plot_disc_pushforward_quantiles(shocks, paste0('pi_idsc_sck[', 1:data$N_stand_species, ']'), display_ylim = c(0,0.5),
                                      ylab = 'p(tree idiosync. sck)')
 
-
+layout(matrix(c(1,2,3,3), ncol = 2, byrow = T))  
+par(mar = c(1,4,4,1), cex.lab = 1)
+util$plot_expectand_pushforward(samples[['phi_sck0']], 50, flim= c(0, 1), main = 'phi_sck0')
+prior <- rbeta(1e6, 2, 20)
+lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
+util$plot_expectand_pushforward(samples[['tau_phi_sck']], 50, flim= c(0,5), main = 'tau_phi_sck')
+prior <- rnorm(1e6, 0, 1)
+lines(density(prior), col = util$c_light_teal, lwd = 1.5, lty = 2)
+par(mar = c(3,4,2,1))
+util$plot_disc_pushforward_quantiles(shocks, paste0('phi_sck[', 1:data$N_stands, ']'), display_ylim = c(0,1),
+                                     ylab = 'p(stand conc.)')
 
 layout(matrix(c(1,2,3,3), ncol = 2, byrow = T))
 par(mar = c(1,4,4,1), cex.lab = 1)
