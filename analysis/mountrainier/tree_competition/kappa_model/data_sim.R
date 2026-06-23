4fd8f49181c1adc35ab905a9af0b24a289715768
Qingyang Deng <qdeng06@students.cs.ubc.ca>
2026-06-23 14:45:33 +0800

    delta model data simulation script updated due to the addition of species data

--- analysis/mountrainier/tree_competition/kappa_model/data_sim.R
+++ analysis/mountrainier/tree_competition/kappa_model/data_sim.R
@@ -7,12 +7,13 @@ options(mc.cores = parallel::detectCores())
 
 #parameter
 set.seed(300)
-
+S_nu <- 3
 sigma <- rnorm(1,0, 0.095 / 2.57) #0.05078215
-y0 <- rnorm(1,0,10/2.57)#3.354501
+y0 <- rnorm(S_nu,0,10/2.57)#3.354501
 k <- rbeta(1,2,2) #0.6317492
-r <- rbeta(1,4,2) #0.4923903
-beta <- log(1.05)
+r <- rbeta(S_nu,4,2) #0.4923903
+#beta <- log(1.05)
+beta <- rnorm(S_nu,0,log(1.1)/2.57)
 
 N <- integer(0)
 b <- numeric(0)
@@ -62,6 +63,7 @@ N <- sample(4:8,Nf,replace=TRUE)
 N_total <- sum(N)
 b <- runif(N_total,2.1,2.5) # x1000cm^2
 focal_corr <- runif(N_total,0,1)
+tree_sp <- sample(1:3,Nf,replace=TRUE)
 
 # start and end index
 start_idx <- numeric(0)
@@ -88,9 +90,9 @@ for (i in 1:Nf) {
   bn <- b[start_idx[i]:end_idx[i]]
   corrn <- focal_corr[start_idx[i]:end_idx[i]]
   baphy[i] = sum(bn*(corrn^k))
-  competition[i]=beta*(baphy[i]-BA_compet0)
-  avails[i] = (bf[i]-bf0)*r
-  mu[i]=log(y0) + avails[i] - competition[i]
+  competition[i]=beta[tree_sp[i]]*(baphy[i]-BA_compet0)
+  avails[i] = (bf[i]-bf0)*r[tree_sp[i]]
+  mu[i]=log(y0[tree_sp[i]]) + avails[i] - competition[i]
 }
 
 
@@ -101,7 +103,9 @@ y <- exp(log_y)
 
 stan_data <- list(
   Nf = Nf,
+  S = S_nu,
   N = N,
+  tree_sp = tree_sp,
   N_total = N_total,
   b = b,
   focal_corr = focal_corr,
@@ -131,3 +135,37 @@ diagnostics <- util$extract_hmc_diagnostics(fit)
 util$check_all_hmc_diagnostics(diagnostics)
 samples <- util$extract_expectand_vals(fit)
 util$check_all_expectand_diagnostics(samples)
+
+# visualization
+util$plot_expectand_pushforward(samples[["sigma"]], 25,
+                                display_name=bquote(sigma~"(standard deviation of tree growth)"),
+                                #baseline=sigma,
+                                baseline_col=util$c_mid_teal) 
+abline(v=sigma)
+
+util$plot_expectand_pushforward(samples[["k"]],25,display_name = bquote(kappa),
+                                baseline_col = util$c_mid_teal)
+abline(v=k)
+
+par(mfrow = c(3,1))
+for (i in 1:3){
+  util$plot_expectand_pushforward(samples[[paste0("y0[",i,"]")]],25,
+                                  display_name = paste0("y0[",i,"]"),
+                                  baseline_col=util$c_mid_teal)
+  abline(v=y0[i])
+}
+
+for(i in 1:3){
+  util$plot_expectand_pushforward(samples[[paste0("beta[",i,"]")]],25,
+                                  display_name = bquote(beta[.(i)]),
+                                  baseline_col = util$c_mid_teal)
+  abline(v=beta[i])
+}
+
+for(i in 1:3){
+  util$plot_expectand_pushforward(samples[[paste0("r[",i,"]")]],25,
+                                  display_name = paste0("r[",i,"]"),
+                                  baseline_col = util$c_mid_teal)
+  abline(v=r[i])
+}
+
