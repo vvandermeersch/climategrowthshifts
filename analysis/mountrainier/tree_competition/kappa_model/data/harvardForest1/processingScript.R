f247715e2684cb29d2dd3acf67bee6df1d9545b6
Qingyang Deng <qdeng06@students.cs.ubc.ca>
2026-08-31 17:46:18 -0700

    HF species codes added to HF data

--- analysis/mountrainier/tree_competition/kappa_model/data/harvardForest1/processingScript.R
+++ analysis/mountrainier/tree_competition/kappa_model/data/harvardForest1/processingScript.R
@@ -8,47 +8,39 @@ library(dbscan)
 
 setwd("D:/ubc_study/udergrad_research/temporal ecology lab/climategrowthshifts/analysis/mountrainier/tree_competition/kappa_model/data/harvardForest1")
 
-#alive stems-> treeid, stemid match -> dbh difference/2
-# tree2014 <- read_csv("hf253-05-stems-2014.csv",show_col_types = FALSE)
-# tree2019 <- read_csv("hf253-06-stems-2019.csv",show_col_types = FALSE)
+# processing species code, clean up individuals with unknown genus
+# unify the unknown species to "spp."
+species_code <- read_csv("hf253-02-species-codes.csv")%>%
+  filter(genus!="Unidentified") %>%
+  mutate(species = ifelse(species %in% c("unknown","unk_hardwood","unk_conifer","species"),
+                          "spp.",species)) %>%
+  mutate(latin = paste0(genus,"_",species))%>%
+  select(sp,latin,genus,species) 
 
-#treeid, stemid, sp, gx, gy, dbh
-# alive status
 stemdataName <- list("hf253-05-stems-2014.csv","hf253-06-stems-2019.csv")
 stem_list <- map(stemdataName,function(f){
   dat <- read_csv(f,show_col_types = FALSE)
   dat%>%
-    filter(status=='A')%>%
+    filter(status=='A') %>%
     select(tree.id,stem.id,sp,gx,gy,dbh)
 })
 
+#treeid, stemid, sp, gx, gy, dbh
+# alive status
+# calculate tree growth from 2014-2019 as the radial growth
 growthchange <- stem_list[[1]]%>%
   inner_join(stem_list[[2]],
              by = c("tree.id","stem.id"),
              suffix = c("_2014","_2019"))%>%
-  filter(sp_2014==sp_2019)%>%
+  filter(sp_2014==sp_2019) %>%
   mutate(growth = (dbh_2019-dbh_2014)/2,
          locationchange = sqrt((gx_2014-gx_2019)^2+(gy_2014-gy_2019)^2))%>%
   filter(locationchange==0)%>%
   select(tree.id,stem.id,sp_2014,gx_2014,gy_2014,growth)%>%
-  rename(sp=sp_2014,gx=gx_2014,gy=gy_2014)
+  rename(sp=sp_2014,gx=gx_2014,gy=gy_2014)%>%
+  inner_join(species_code,by="sp")
 
-# neighborhood_2014 <- stem_list[[1]] %>%
-#   select(focal_treeid = tree.id,
-#          focal_stemid = stem.id,
-#          focal_x = gx,
-#          focal_y = gy) %>%
-#   crossing(
-#     stem_list[[1]] %>%
-#       select(neighbor_treeid = tree.id,
-#              neighbor_stemid = stem.id,
-#              neighbor_sp = sp,
-#              neighbor_x = gx,
-#              neighbor_y = gy,
-#              neighbor_dbh = dbh)
-#   ) %>%
-#   mutate(dist = sqrt((neighbor_x-focal_x)^2+(neighbor_y-focal_y)^2))%>%
-#   filter(dist > 0 & dist <= 10)
+# calculate the neighborhood in 2014 and 2019
 neighborhood_list <- map(stem_list,function(d) {
   dat <- d %>% filter(!is.na(gx),!is.na(gy))
   coords <- as.matrix(dat[,c("gx","gy")])
@@ -80,6 +72,19 @@ neighborhood_list <- map(stem_list,function(d) {
   return(neighborhood)
 })
 
-write.csv(neighborhood_list[[1]],"processed/neighborhood_2014.csv")
-write.csv(neighborhood_list[[2]],"processed/neighborhood_2019.csv")
+# find the latin name of neighboring species
+neighbor_2014 <- neighborhood_list[[1]] %>%
+  inner_join(species_code,by = c("neighbor_sp"="sp"))%>%
+  rename(neighbor_latin = latin,
+         neighbor_genus = genus,
+         neighbor_species = species)
+neighbor_2019 <- neighborhood_list[[2]] %>%
+  inner_join(species_code,by = c("neighbor_sp"="sp")) %>%
+  rename(neighbor_latin = latin,
+         neighbor_genus = genus,
+         neighbor_species = species)
+
+# save processed files
+write.csv(neighbor_2014,"processed/neighborhood_2014.csv")
+write.csv(neighbor_2019,"processed/neighborhood_2019.csv")
 write.csv(growthchange,"processed/tree_growth.csv")
